3d8fe21acaa2038e2c9a2ed5ccb4a40ccc388cea
Courtland <crouchetcourtland@gmail.com>
2022-09-16 14:54:12 -0500

    update gen element

--- generateElement.js
+++ generateElement.js
@@ -1,19 +1,30 @@
 //General use function for creating a DOM element
-function generateElement(parentEl=document.body, type="div", id="", classStr="", content="")
-{
-    let el = document.createElement(type);
-    el.id = id;
-    el.className = classStr;
-    if(typeof(content) == "string") el.innerHTML = content;
-    else if(typeof(content) == "object") el.appendChild(content);
-    if(isDefined(parentEl)) parentEl.appendChild(el);
-    return el;
+function generateElement(
+  parentEl = document.body,
+  type = "div",
+  id = "",
+  classStr = "",
+  content = "",
+  attribute = null,
+  attributeValue = null,
+  attribute2 = null,
+  attribute2Value = null
+) {
+  let el = document.createElement(type);
+  el.id = id;
+  el.className = classStr;
+  el.setAttribute(attribute, attributeValue);
+  el.setAttribute(attribute2, attribute2Value);
+  if (typeof content == "string") el.innerHTML = content;
+  else if (typeof content == "object") el.appendChild(content);
+  if (isDefined(parentEl)) parentEl.appendChild(el);
+  return el;
 }
 function isDefined(obj) {
-    return obj !== undefined && obj != null;
+  return obj !== undefined && obj != null;
 }
 
 module.exports = {
-    generateElement,
-    isDefined
-}
+  generateElement,
+  isDefined,
+};
